642424c84840b356903af7ae2b4df341cd92b217
Gilangp <gilangpurnomo0505@gmail.com>
2025-09-21 14:24:41 +0700

    add: week 4 dan praktikum 1

---
+++ codelab04_collections_and_funtions/src/praktikum1.dart
@@ -0,0 +1,20 @@
+void main() {
+  // var list = [1, 2, 3];
+  // assert(list.length == 3);
+  // assert(list[1] == 2);
+  // print(list.length);
+  // print(list[1]);
+
+  // list[1] = 1;
+  // assert(list[1] == 1);
+  // print(list[1]);
+
+  // Langkah 3
+  final list = [null, "Gilang Purnomo", "2341720042", null, null ];
+  assert(list.length == 5);
+  assert(list[1] == "Gilang Purnomo");
+  print(list.length);
+  print(list[1]);
+  print(list[2]);
+  print(list);
+}
